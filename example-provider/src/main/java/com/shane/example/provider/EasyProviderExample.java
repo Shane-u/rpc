fa29b50aa5038422b4e33fb0d5fb911bb1fa3a65
Shane <1554096735@qq.com>
2025-08-30 14:12:15 +0800

    Add LocalRegistry for service registration and update EasyProviderExample

--- example-provider/src/main/java/com/shane/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/shane/example/provider/EasyProviderExample.java
@@ -1,5 +1,7 @@
 package com.shane.example.provider;
 
+import com.shane.example.common.service.UserService;
+import com.shane.rpc.registry.LocalRegistry;
 import com.shane.rpc.server.HttpServer;
 import com.shane.rpc.server.VertxHttpServer;
 
@@ -12,6 +14,8 @@ import com.shane.rpc.server.VertxHttpServer;
 public class EasyProviderExample {
 
     public static void main(String[] args) {
+        // 注册服务
+        LocalRegistry.register(UserService.class.getName(),UserServiceImpl.class);
         // 启动web服务
         HttpServer vertxHttpServer = new VertxHttpServer();
         vertxHttpServer.doStart(8080);
