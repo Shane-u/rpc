d7df2433c8319f6debaf399e45a624ca9487e0dd
Shane <1554096735@qq.com>
2025-08-30 15:49:11 +0800

    Add dynamic proxy implementation for RPC requests with ServiceProxy and ServiceProxyFactory

--- example-consumer/src/main/java/com/shane/example/consumer/EasyConsumerExample.java
+++ example-consumer/src/main/java/com/shane/example/consumer/EasyConsumerExample.java
@@ -2,6 +2,7 @@ package com.shane.example.consumer;
 
 import com.shane.example.common.model.User;
 import com.shane.example.common.service.UserService;
+import com.shane.rpc.proxy.ServiceProxyFactory;
 
 /**
  * 简易服务消费者示例
@@ -13,7 +14,10 @@ public class EasyConsumerExample {
 
     public static void main(String[] args) {
         // 需要获得 UserService 的实现类对象
-        UserService userService = new UserServiceProxy();
+        // 静态代理
+        // UserService userService = new UserServiceProxy();
+        // 动态代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
         User user = new User();
         user.setName("shane");
         // 调用
